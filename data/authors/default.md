68276b439ac609d3e1e7efe09e67fa346110a56e
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-07-11 21:37:57 +0530

    aded new commit

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,15 +1,15 @@
 ---
-name: Tails Azimuth
+name: Ajay Dhangar
 avatar: /static/images/avatar.png
-occupation: Professor of Atmospheric Science
-company: Stanford University
-email: address@yoursite.com
-twitter: https://twitter.com/Twitter
-linkedin: https://www.linkedin.com
-github: https://github.com
+occupation: Software Development Engineer
+company: CodeMastermindHQ
+email: ajaydhangar49@gmail.com
+twitter: https://twitter.com/AJAYDHA27250016
+linkedin: https://www.linkedin.com/in/ajay-dhangar/
+github: https://github.com/Ajay-Dhangar
 ---
 
-Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds.
+Ajay Dhangar is a Software Development Engineer at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds.
 
 He leads the clean energy group which develops 3D air pollution-climate models, writes differential equation solvers, and manufactures titanium plated air ballons. In his free time he bakes raspberry pi.
 
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: "Coding World's Blog",
+  author: 'Ajay Dhangar',
+  headerTitle: 'Coding World',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
@@ -10,12 +10,12 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  email: 'ajaydhangar49@gmail.com',
+  github: 'https://github.com/Ajay-Dhangar',
+  twitter: 'https://twitter.com/AJAYDHA27250016',
   facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  youtube: 'https://youtube.com/CodeMastermindHQ',
+  linkedin: 'https://www.linkedin.com/in/ajay-dhangar/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
